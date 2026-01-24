116fe37054989f347fc714acb19386cc0b14299d
Obi Wan Jabroni <obiwanjabroni271@gmail.com>
2026-01-24 15:53:07 +1000

    Fix auth - remove prerender, add client-side Supabase layout

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,2 +1,42 @@
-// Make all pages prerenderable
-export const prerender = true;
+import { createBrowserClient, createServerClient, isBrowser } from '@supabase/ssr';
+import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from '$env/static/public';
+import type { LayoutLoad } from './$types';
+
+export const load: LayoutLoad = async ({ data, depends, fetch }) => {
+	// Skip if Supabase isn't configured
+	if (!PUBLIC_SUPABASE_URL || !PUBLIC_SUPABASE_ANON_KEY) {
+		return {
+			...data,
+			supabase: null,
+			session: null
+		};
+	}
+
+	// Declare a dependency on Supabase authentication state
+	depends('supabase:auth');
+
+	// Create a Supabase client based on the environment (browser or server)
+	const supabase = isBrowser()
+		? createBrowserClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY, {
+				global: { fetch }
+			})
+		: createServerClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY, {
+				global: { fetch },
+				cookies: {
+					getAll() {
+						return [];
+					}
+				}
+			});
+
+	// Get session
+	const {
+		data: { session }
+	} = await supabase.auth.getSession();
+
+	return {
+		...data,
+		supabase,
+		session
+	};
+};
